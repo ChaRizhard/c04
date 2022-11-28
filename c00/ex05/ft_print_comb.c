465367df481b3eb34599ae55d96257ba563fba96
ChaRizhard <66208087+ChaRizhard@users.noreply.github.com>
2022-11-28 14:10:18 +1030

    Fixed up c00 ex05 to have <25 lines of code

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -1,37 +1,47 @@
 #include <unistd.h>
 
+void ft_putchar(char c)
+{
+    write (1, &c, 1);
+}
+
+void ft_printNum(int a, int b, int c)
+{
+    ft_putchar(a);
+    ft_putchar(b);
+    ft_putchar(c);
+    ft_putchar(',');
+    ft_putchar(' ');
+}
+
 void ft_print_comb(void)
 {
-	int a;
-	int b;
-	int c;
+        int a;
+        int b;
+        int c;
 
-	a = '0';
+        a = '0';
 
-	while (a <= '7')
-	{
-		b = a + 1;
-		while (b <= '8')
-		{
-			c = b + 1;
-			while (c <= '9')
-			{
-				write (1, &a, 1);
-				write (1, &b, 1);
-				write (1, &c, 1);
-				if (a != '7' || b != '8' || c != '9')
-                    write(1, ", ", 2);
-                c++;
-			}
-			b++;
-		}
-		a++;
-	}
+        while (a <= '7')
+        {
+                b = a + 1;
+                while (b <= '8')
+                {
+                        c = b + 1;
+                        while (c <= '9')
+                        {
+                                ft_printNum(a, b, c);
+                                c++;
+                        }
+                        b++;
+                }
+                a++;
+        }
 
 }
 
 int main(void)
 {
-	ft_print_comb();
-	return(0);
+        ft_print_comb();
+        return(0);
 }
