62def066084a80e76e6cebb680ad47dd3ff29a09
ChaRizhard <66208087+ChaRizhard@users.noreply.github.com>
2022-12-01 22:45:35 +1030

    Edited ex03 so main() would look cleaner

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -9,20 +9,11 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 
 int main()
 {
-	int a;
-	int b;
-	int c;
-	int d;
+	int a, b, c, d;
 	a = 21;
 	b = 5;
 
 	ft_div_mod(a, b, &c, &d);
-	printf("%i ", a);
-	printf("Divided by ");
-	printf("%i ", b);
-	printf("equals to ");
-	printf("%i ", c);
-	printf("remainder ");
-	printf("%i ", d);
+	printf("%i Divided by %i equals %i remainder %i", a, b, c, d);
 	return(0);
 }
