dd6707efd43226acb2d50940bbb207bcb093f8c7
ChaRizhard <66208087+ChaRizhard@users.noreply.github.com>
2022-12-12 10:18:49 +1030

    Fixed ex00

--- 06December/c03/ex00/ft_strcmp.c
+++ 06December/c03/ex00/ft_strcmp.c
@@ -5,26 +5,26 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 	i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] > s2[i])
 		{
-			return (s1[i] - s2[i]);
+			return (1);
+		}
+		if (s1[i] < s2[i])
+		{
+			return(-1);
 		}
 		i++;
 	}
-	return(s1[i] - s2[i]);
+	return(0);
 }
 /*
 int	main()
 {
-	char	a[100];
-	char	b[100];
-
-	strcpy(a, "Ab1");
-	strcpy(b, "Ab2");
-	printf("First: %i\n", ft_strcmp(a, b));
-	printf("Second: %i\n", ft_strcmp(a, b));
+	printf("%d\n", ft_strcmp("abc", "abc"));
+	printf("%d\n", ft_strcmp("abc", "ab"));
+	printf("%d\n", ft_strcmp("ab", "abc"));
 	return(0);
 }
 */
