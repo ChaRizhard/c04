e74ff114f94768cab6bf36df43a628f54f4edae8
ChaRizhard <66208087+ChaRizhard@users.noreply.github.com>
2022-12-12 12:26:04 +1030

    Upload ex00-ex02

---
+++ 12December/c04/ex02/ft_putnbr.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+int	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write (1, str++, 1);
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putstr('-2147483648');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+
+}
+/*
+int	main()
+{
+	ft_putnbr(-42);
+	return(0);
+}
+*/
